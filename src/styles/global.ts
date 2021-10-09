d70c33a5f89a94a6d556c0f5fb828c839660e6f5
Andre <afelipp3@gmail.com>
2021-10-09 12:51:37 -0300

    update global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,9 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html {
@@ -14,6 +17,8 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    font-size: 1.6rem;
   }
 `
 
