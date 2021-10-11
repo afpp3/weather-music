76e22c582f0fdff54131090e92949877ec776fa2
Andre <afelipp3@gmail.com>
2021-10-11 11:25:19 -0300

    Create Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>
+    <p>André Felippe 2021 © All rights reserved</p>
+  </S.Wrapper>
+)
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.4rem;
+  height: 8rem;
+  margin-top: 10rem;
+  background-color: #4040a1;
+  color: #d4f1f4;
+`
