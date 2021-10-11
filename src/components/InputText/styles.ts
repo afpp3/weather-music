859226f67e1785b93eb04d1392eaa1802b4e09ff
Andre <afelipp3@gmail.com>
2021-10-11 11:26:29 -0300

    change input text style

--- src/components/InputText/styles.ts
+++ src/components/InputText/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Input = styled.input`
-  background-color: hsl(220, 12%, 95%);
+  background-color: hsl(220, 12%, 92%);
   border: none;
   border-radius: 0.5rem;
   box-shadow: inset 0 2px 4px 0 hsla(0, 0%, 0%, 0.08);
