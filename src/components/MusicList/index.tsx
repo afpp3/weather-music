ce327358e43988a2a73b9b611bd3e9d2f5db24cd
Andre <afelipp3@gmail.com>
2021-10-14 17:30:24 -0300

    change music list

--- src/components/MusicList/index.tsx
+++ src/components/MusicList/index.tsx
@@ -3,32 +3,37 @@ import * as S from './styles'
 import Button from 'components/Button'
 
 export type Music = {
-  name: string
+  title: string
 }
 
 export type MusicListProps = {
-  musics: Music[]
+  musics?: Music[]
+  onSave?: () => void
 }
 
-const MusicList = ({ musics }: MusicListProps) => (
-  <S.Wrapper>
-    <S.Heading>
-      <h2>Músicas sugeridas</h2>
-    </S.Heading>
-    <S.List>
-      {musics ? (
-        musics.map((music, index) => <S.Item key={index}>{music.name}</S.Item>)
-      ) : (
-        <span>Nenhuma música encontrada!</span>
-      )}
-    </S.List>
-    <S.Actions>
-      <Link href="/music-list" passHref>
-        <S.SavedLists>Listas salvas</S.SavedLists>
-      </Link>
-      <Button>Salvar</Button>
-    </S.Actions>
-  </S.Wrapper>
-)
+const MusicList = ({ musics, onSave }: MusicListProps) => {
+  return (
+    <S.Wrapper>
+      <S.Heading>
+        <h2>Músicas sugeridas</h2>
+      </S.Heading>
+      <S.List>
+        {musics?.length ? (
+          musics.map((music, index) => (
+            <S.Item key={index}>{music.title}</S.Item>
+          ))
+        ) : (
+          <span>Nenhuma música encontrada!</span>
+        )}
+      </S.List>
+      <S.Actions>
+        <Link href="/music-list" passHref>
+          <S.SavedLists>Listas salvas</S.SavedLists>
+        </Link>
+        <Button handleClick={onSave}>Salvar lista</Button>
+      </S.Actions>
+    </S.Wrapper>
+  )
+}
 
 export default MusicList
